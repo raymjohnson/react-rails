afc21401f7afee31b8b7103cdd52e50938282224
Ryan Bigg <me@ryanbigg.com>
2019-02-26 16:28:27 +1100

    Remove test directory

--- test/controllers/rating_questions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RatingQuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get rating_questions_index_url
-    assert_response :success
-  end
-
-end
--- test/models/rating_question_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RatingQuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
