348a64a46dad2c868183624c9d661c0477e0feb1
Rachel Johnson <rachel.johnson@cultureamp.com>
2019-03-19 11:53:44 +1100

    Initial commit

--- app/controllers/rating_questions_controller.rb
+++ app/controllers/rating_questions_controller.rb
@@ -1,57 +1,37 @@
 class RatingQuestionsController < ApplicationController
-  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
+  before_action :find_rating_question, only: [:show, :update, :edit, :destroy]
 
   def index
     @rating_questions = RatingQuestion.all
   end
 
-  def new
-    @rating_question = RatingQuestion.new
-  end
+  def show; end
 
   def create
     @rating_question = RatingQuestion.new(question_params)
     if @rating_question.save
       respond_to do |format|
-        format.html { redirect_to @rating_question, notice: "Your question has been created." }
-        format.json { render :show, status: :created }
+        format.html { render :show, notice: "Successfully created question: #{@rating_question[:title]}" }
+        format.json { render :show, status: 201 }
       end
     else
-      errors = @rating_question.errors.full_messages.to_sentence
       respond_to do |format|
-        format.html { render :new, notice: errors }
-        format.json { render json: { 'errors' => @rating_question.errors.messages }, status: :unprocessable_entity }
+        format.html { render :new, notice: { errors: @rating_question.errors.messages } }
+        format.json { render json: { errors: @rating_question.errors.messages }, status: 422 }
       end
     end
-  end
-
-  def edit
   end
 
   def update
-    if @rating_question.update(question_params)
-      respond_to do |format|
-        format.html { redirect_to @rating_question, notice: "Updated Successfully" }
-        format.json { render :show , status: :ok }
-      end
-    else
-      errors = @rating_question.errors.full_messages.to_sentence
-      respond_to do |format|
-        format.html { render :edit, notice: errors }
-      end
-    end
+    @rating_question.update(question_params)
+    render :show
   end
+
+  def edit; end
 
   def destroy
-    if @rating_question.destroy
-      respond_to do |format|
-        format.html { redirect_to "/", notice: "Deleted Successfully" }
-        format.json { render json: {}, status: :no_content }
-      end
-    end
-  end
-
-  def show
+    @rating_question.destroy
   end
 
   private
@@ -60,12 +40,10 @@ class RatingQuestionsController < ApplicationController
     params.require(:rating_question).permit(:title, :tag)
   end
 
-  def find_question
+  def find_rating_question
     @rating_question = RatingQuestion.find(params[:id])
-    unless @rating_question
-      head :not_found
-      return
-    end
+    head 404 unless @rating_question
   end
-  
+
 end
+
