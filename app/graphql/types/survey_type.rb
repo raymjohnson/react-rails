814154827e7ea1fb665f00e7a0bbd3ddaa7d5636
Rachel Johnson <rachel.johnson@cultureamp.com>
2019-04-30 17:12:55 +1000

    Query questions within a survey

--- app/graphql/types/survey_type.rb
+++ app/graphql/types/survey_type.rb
@@ -2,5 +2,6 @@ module Types
   class SurveyType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :rating_questions, [QuestionType], null: false
   end
 end
