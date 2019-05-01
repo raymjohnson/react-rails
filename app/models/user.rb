f5da67259d725cda4bfc95f793d4e7af20378bac
Rachel Johnson <rachel.johnson@cultureamp.com>
2019-05-01 13:37:58 +1000

    Add Active Models has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User
   field :email, type: String
   field :password_digest, type: String
 
+  has_secure_password
+
   validates(:name, presence: true)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false })
-  has_secure_password
-  validates(:password, presence: true, length: { minimum: 8 }, allow_nil: true)
+                    format: { with: VALID_EMAIL_REGEX })
+  validates(:password, presence: true, length: { minimum: 6 }, allow_nil: true)
 end
